2df81ecaed1e7de76340d36dcde4dbf4d7f8f2cc
Mateuszto <38840598+Mateuszto@users.noreply.github.com>
2019-01-27 15:23:09 +0100

    Login page
    Login page, and login - When someone is logged, its skipping first page with form.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './sass/style.sass'
+
+const sectionWelcome = document.querySelector('.welcome--page');
+const welcomeInput = document.querySelector('.welcome--input');
+const welcomeForm = document.querySelector('.welcome--form');
+
+let nickName = welcomeInput.value;
+let showNick = document.querySelector('.main--nick');
+
+
+if (localStorage.hasOwnProperty('name')) {
+    sectionWelcome.classList.add('unactiveForm');
+    showNick.textContent = localStorage.getItem('name');
+}
+
+
+const welcomeUser = (e) => {
+    e.preventDefault();
+
+    if (welcomeInput.value === "") {
+        return false
+    } else {
+        sectionWelcome.classList.add('unactiveForm');
+        localStorage.setItem('name', welcomeInput.value)
+        showNick.textContent = localStorage.getItem('name');
+    }
+}
+
+welcomeForm.addEventListener('submit', welcomeUser);
